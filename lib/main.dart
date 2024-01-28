f00b5430d63c2716b541fa3a4a3cb68fabce5904
Amonle Merry <amonlemerry@gmail.com>
2024-01-28 09:35:22 +0100

    Flutter projet 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-import 'HomePage.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:picturetranscriptor/transcription.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+late List<CameraDescription> _cameras;
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
   runApp(MyApp());
 }
 
@@ -17,5 +23,132 @@ class MyApp extends StatelessWidget {
 }
 
 
+class HomePage extends StatelessWidget {
+  final ImagePicker _picker = ImagePicker();
+  final textDetector =TextRecognizer(script: TextRecognitionScript.latin);
+  Future<void> _getImageFromGallery() async {
+    try {
+      XFile? imageFile = await _picker.pickImage(source: ImageSource.gallery);
+      if (imageFile != null) {
+        final inputImage = InputImage.fromFilePath(imageFile.path);
+        final recognisedText = await textDetector.processImage(inputImage);
+        final text = recognisedText.text;
+        Transcription transcription=new Transcription(imageFile.path, text);
+        TranscriptionList.savetranscription(transcription);
+      }
+    } catch (e) {
+      print('Une erreur est survenue lors de la récupération de l\'image : $e');
+    }
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(), // Remplacez par votre widget
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              heroTag: "cameraButton", // Ajout d'un tag unique
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CameraScreen()),
+                );
+              },
+              tooltip: 'Prendre une photo',
+              child: Icon(Icons.camera),
+            ),
+            SizedBox(width: 10), // Espacement entre les boutons
+            FloatingActionButton(
+              heroTag: "photoLibraryButton", // Ajout d'un tag unique
+              onPressed: _getImageFromGallery,
+              tooltip: 'Importer une photo',
+              child: Icon(Icons.photo_library),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  final TextRecognizer textRecognitionProcessor = GoogleMlKit.vision.textRecognizer();
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+          // Handle access errors here.
+            break;
+          default:
+          // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> takePicture() async {
+    try {
+      final image = await controller.takePicture();
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>TranscritionView(picture: image)));
+    } on CameraException catch (e) {
+      print('Une erreur est survenue lors de la prise de photo : ${e.description}');
+      // Vous pouvez ajouter ici un code pour gérer l'erreur de manière plus appropriée, par exemple en affichant un message à l'utilisateur.
+    } catch (e) {
+      print('Une erreur inattendue est survenue : $e');
+      // Gérer les autres erreurs non spécifiques à la caméra.
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+      aspectRatio: controller!.value.aspectRatio,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          CameraPreview(controller!),
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: FloatingActionButton(
+              child: Icon(Icons.camera),
+              onPressed: takePicture,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
--- lib/transcription.dart
+++ lib/transcription.dart
@@ -1,4 +1,62 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:picturetranscriptor/main.dart';
+
+
+class TranscritionView extends StatefulWidget {
+  TranscritionView({required this.picture, Key? key}) : super(key: key);
+  XFile picture;
+  @override
+  State<TranscritionView> createState() => _TranscritionViewState();
+}
+
+class _TranscritionViewState extends State<TranscritionView> {
+  late File imageFile;
+  late InputImage inputImage;
+
+  @override
+  void initState(){
+    super.initState();
+    load();
+  }
+ void load() async{
+   imageFile = File(widget.picture.path);
+   TextRecognizer textRecognizer = TextRecognizer();
+   inputImage = InputImage.fromFilePath(widget.picture.path);
+   RecognizedText recognisedText = await textRecognizer.processImage(inputImage);
+   Transcription transcription=new Transcription(imageFile.path, recognisedText.text);
+   TranscriptionList.setState(transcription);
+   print(TranscriptionList._transcriptions);
+ }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+                child: Image.network(imageFile.path)
+            ),
+            Container(
+              child: Text(''),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (builder)=>MyApp()));
+          },
+        child: Icon(Icons.home),
+      ),
+    );
+  }
+}
+
+
+
 
 
 
@@ -13,11 +71,14 @@ class Transcription {
 class TranscriptionList extends StatelessWidget {
   static List<Transcription> _transcriptions=[];
 
+  static setState(Transcription transcription)
+  {
+    savetranscription(transcription);
+  }
   static void savetranscription(Transcription transcription)
   {
     _transcriptions.add(transcription);
   }
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
